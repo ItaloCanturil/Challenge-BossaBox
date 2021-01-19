c1743ee95aeaedff5333ab07136047333bc51c08
Canturilsnaik <italo.cantur@gmail.com>
2021-01-18 23:49:45 -0300

    :wrench: Add async function in api file

--- services/api.js
+++ services/api.js
@@ -4,4 +4,19 @@ const axiosInstance = axios.create({
   url: 'http://localhost:3000/tools'
 })
 
-export default axiosInstance
+async function getAllTools() {
+  return axios.get('/tools')
+    .then((response) => response)
+    .catch((err) => console.log('getAllTools', err))
+}
+
+async function searchTools() {
+  return axios.get('tools?title=')
+    .then((response) => response)
+    .catch((err) => console.error('getAllTools', err))
+}
+export default {
+  axiosInstance,
+  getAllTools,
+  searchTools
+}
