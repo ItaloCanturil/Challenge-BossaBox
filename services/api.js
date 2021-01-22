71c807f44c081644d0e76947c26d879e3550fbbd
Canturilsnaik <italo.cantur@gmail.com>
2021-01-22 12:43:17 -0300

    :wrench: Fix with eslint

--- services/api.js
+++ services/api.js
@@ -4,15 +4,4 @@ const axiosInstance = axios.create({
   baseURL: 'http://localhost:3000/'
 })
 
-// async function getAllTools() {
-//   return axios.get('/tools')
-//     .then((response) => response)
-//     .catch((err) => console.log('getAllTools', err))
-// }
-
-// async function searchTools() {
-//   return axios.get('tools?title=')
-//     .then((response) => response)
-//     .catch((err) => console.error('getAllTools', err))
-// }
 export default axiosInstance
